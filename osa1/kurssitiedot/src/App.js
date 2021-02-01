12315d6d67cbdd61897046e742a360d8c552e599
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2021-02-01 18:32:08 +0200

    osa1 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <Header course={course}/>
-      <Content 
+      <Header course={course} />
+      <Content
         part1={part1}
         exercises1={exercises1}
         part2={part2}
@@ -20,35 +20,44 @@ const App = () => {
         part3={part3}
         exercises3={exercises3}
       />
-      <Total 
+      <Total
         exercises1={exercises1}
         exercises2={exercises2}
         exercises3={exercises3}
       />
     </div>
-  )
-}
+  );
+};
 
 const Header = (props) => {
-  return (
-    <h1>{props.name}</h1>
-  )
-}
+  return <h1>{props.name}</h1>;
+};
 
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
-  )
-}
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
 
-export default App
+export default App;
