0077acaeecee6b8494afe8a812e9eec6fed51dca
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2021-02-01 19:11:53 +0200

    osa1 1.10

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -9,34 +9,38 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-
+      <Button text={"good"} handler={() => setGood(good + 1)}/>
+      <Button text={"neutral"} handler={() => setNeutral(neutral + 1)}/>
+      <Button text={"bad"} handler={() => setBad(bad + 1)}/>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
 
+const Button = (props) => {
+  const { text, handler } = props;
+  return (
+    <button onClick={handler}>{text}</button>
+  )
+}
+
 const Statistics = (props) => {
   const { good, neutral, bad } = props;
 
   const all = good + bad + neutral
-  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const average = `${(good * 1 + neutral * 0 + bad * -1) / all}%`
   const positive = (good / all) * 100
   return (
     <div>
       <h1>statistics</h1>
       { all > 0 && (
         <div>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {all}</p>
-          <p>
-            average {average}
-          </p>
-          <p>positive {positive}%</p>
+          <StatisticsLine text={"good"} value={good} />
+          <StatisticsLine text={"neutral"} value={neutral} />
+          <StatisticsLine text={"bad"} value={bad} />
+          <StatisticsLine text={"all"} value={all} />
+          <StatisticsLine text={"average"} value={average} />
+          <StatisticsLine text={"positive"} value={positive} />
         </div>
       )}
       { all == 0 && (
@@ -45,5 +49,12 @@ const Statistics = (props) => {
     </div>
   );
 };
+
+const StatisticsLine = (props) => {
+  const { text, value } = props;
+  return (
+    <p>{text} {value}</p>
+  )
+}
 
 export default App;
