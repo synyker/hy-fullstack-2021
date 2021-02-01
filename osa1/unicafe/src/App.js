354af4f56fe1b644c5e8371e5f3b28f64f62fcfc
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2021-02-01 18:53:12 +0200

    osa1 1.6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
-    </div>
-  )
-}
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
 
-export default App
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+export default App;
