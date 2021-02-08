45d7a589933c3b3c180b8415768c079963de8d6b
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2021-02-08 19:30:28 +0200

    osa2 2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -11,9 +11,11 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    personService.getAll().then((response) => {
-      setPersons(response.data);
-    });
+    personService
+      .getAll()
+      .then((response) => {
+        setPersons(response.data);
+      })
   }, []);
   console.log("render", persons.length);
   const personsToShow =
@@ -55,27 +57,39 @@ const App = () => {
   };
 
   const updatePerson = (newPerson) => {
-    personService.update(newPerson).then((response) => {
-      setPersons(
-        persons.map((person) => (person.id !== newPerson.id ? person : response.data))
-      );
-      setSuccess(`Updated person ${newPerson.name}`)
-      setTimeout(() => { setSuccess(null)}, 5000)
-    });
+    personService
+      .update(newPerson)
+      .then((response) => {
+        setPersons(
+          persons.map((person) => (person.id !== newPerson.id ? person : response.data))
+        );
+        setSuccess(`Updated person ${newPerson.name}`)
+        setTimeout(() => { setSuccess(null)}, 5000)
+      })
+      .catch(error => {
+        setError(`failed to update person ${newPerson.name}`)
+        setTimeout(() => { setError(null)}, 5000)
+      });
   };
 
-  const deletePerson = (id) => {
+  const deletePerson = (personToDelete) => {
     const handler = () => {
-      console.log(id);
-      personService.remove(id).then((response) => {
-        setPersons(
-          persons.filter((person) => {
-            return person.id !== id;
-          })
-        );
-        setSuccess(`Deleted person ${id}`)
-        setTimeout(() => { setSuccess(null)}, 5000)
-      });
+      console.log(personToDelete.id);
+      personService
+        .remove(personToDelete.id)
+        .then((response) => {
+          setPersons(
+            persons.filter((person) => {
+              return person.id !== personToDelete.id;
+            })
+          );
+          setSuccess(`Deleted person ${personToDelete.name}`)
+          setTimeout(() => { setSuccess(null)}, 5000)
+        })
+        .catch(error => {
+          setError(`failed to delete person ${personToDelete.name}`)
+          setTimeout(() => { setError(null)}, 5000)
+        });
     };
     return handler;
   };
@@ -151,7 +165,7 @@ const Person = (props) => {
       <span>
         {person.name} {person.number}
       </span>
-      <button onClick={deleteHandler(person.id)}>delete</button>
+      <button onClick={deleteHandler(person)}>delete</button>
     </div>
   );
 };
